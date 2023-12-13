bd3e0a89b6b06c524468e8a24fc9098f01527d02
Joel Ansbro <joelmatthewansbro@gmail.com>
2023-12-13 23:49:24 +0000

    First working full script

--- ParseStory.cs
+++ ParseStory.cs
@@ -1,6 +1,7 @@
 using FixerGame;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 using System.IO;
 using System;
 
@@ -13,88 +14,147 @@ public class EventHolder
     public int DifficultyModifier { get; set; }
     public int MaximumAgents { get; set; }
     public int MinimumAgents { get; set; }
-    public List<String> RewardList { get; set; }
-    public int RewardMoney { get; set; }
-    public int RewardExp { get; set; }
+    public RewardList RewardList { get; set; }
 
     public string startEventId { get; set; }
     public Dictionary<string, StoryEvent> Events { get; set; }
-}
 
+    public string getStartStats()
+    {
+        return $"Story ID: {StoryId}\n" +
+               $"Story Name: {StoryName}\n" +
+               $"Story Description: {StoryDescription}\n" +
+               $"Difficulty Modifier: {DifficultyModifier}\n" +
+               $"Maximum Agents: {MaximumAgents}\n" +
+               $"Minimum Agents: {MinimumAgents}\n" +
+               $"Reward List: {RewardList}\n" +
+               $"Start Event ID: {startEventId}\n";
+    }
+
+    public static EventHolder LoadEventHolder(string json)
+{
+    JObject jObject = JObject.Parse(json);
+    JArray eventsArray = jObject[nameof(Events)] as JArray ?? throw new JsonException("Invalid Events array");
+
+    EventHolder eventHolder = new()
+    {
+        StoryId = jObject["StoryId"]?.Value<string>() ?? throw new JsonException("Invalid StoryId"),
+        StoryName = jObject["StoryName"]?.Value<string>() ?? throw new JsonException("Invalid StoryName"),
+        StoryDescription = jObject["StoryDescription"]?.Value<string>() ?? throw new JsonException("Invalid StoryDescription"),
+        DifficultyModifier = jObject["DifficultyModifier"]?.Value<int>() ?? throw new JsonException("Invalid DifficultyModifier"),
+        MaximumAgents = jObject["MaximumAgents"]?.Value<int>() ?? throw new JsonException("Invalid MaximumAgents"),
+        MinimumAgents = jObject["MinimumAgents"]?.Value<int>() ?? throw new JsonException("Invalid MinimumAgents"),
+        RewardList = jObject["RewardList"]?.ToObject<RewardList>() ?? throw new JsonException("Invalid RewardList"),
+        startEventId = jObject["startEventId"]?.Value<string>() ?? throw new JsonException("Invalid startEventId"),
+        Events = new Dictionary<string, StoryEvent>()
+    };
+
+    foreach (JObject eventObject in eventsArray.Cast<JObject>())
+    {
+        StoryEvent storyEvent = StoryEvent.LoadStoryEvent(eventObject.ToString());
+        if (storyEvent is DecisionEvent decisionEvent)
+        {
+            Console.WriteLine("Adding decision event");
+            Console.WriteLine(decisionEvent.Name);
+            Console.WriteLine(decisionEvent.Description);
+            Console.WriteLine(decisionEvent.NodeType);
+            eventHolder.Events.Add(decisionEvent.Name, decisionEvent);
+        } else if (storyEvent is CombatEvent combatEvent)
+        {
+            Console.WriteLine("Adding combat event");
+            Console.WriteLine(combatEvent.Name);
+            Console.WriteLine(combatEvent.Description);
+            Console.WriteLine(combatEvent.NodeType);
+            eventHolder.Events.Add(combatEvent.Name, combatEvent);
+        } else if (storyEvent is EndEvent endEvent)
+        {
+            Console.WriteLine("Adding end event");
+            Console.WriteLine(endEvent.Name);
+            Console.WriteLine(endEvent.Description);
+            Console.WriteLine(endEvent.NodeType);
+            eventHolder.Events.Add(endEvent.Name, endEvent);
+        } else{
+            throw new ArgumentException($"Invalid node type: {storyEvent.NodeType}");
+        }
+    }
+    return eventHolder;
+    }
+}
 
 abstract public class StoryEvent
 {
     public string Name { get; set; }
     public string Description { get; set; }
     public string NodeType { get; set; }
-}
 
-public class StoryEventIntermediate
-{
-    public string NodeType { get; set;}
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
-
-public class StoryEventDeserializer {
-    public static StoryEvent DeserializeStoryEvent(string json)
+    public string getStats()
     {
-        var intermediate = JsonConvert.DeserializeObject<StoryEventIntermediate>(json);
-        var storyEvent = StoryEventFactory.CreateStoryEvent(intermediate.NodeType);
-        JsonConvert.PopulateObject(json, storyEvent);
+        return $"Name: {Name}\n" +
+               $"Description: {Description}\n" +
+               $"Node Type: {NodeType}\n";
+    }
 
-        return storyEvent;
+    public static StoryEvent LoadStoryEvent(string json)
+    {
+    JObject jObject = JObject.Parse(json);
+
+    string nodeType = jObject[nameof(NodeType)]?.Value<string>() ?? throw new JsonException("Invalid node type");
+
+    Console.WriteLine(nodeType);
+    switch (nodeType)
+    {
+        case "Decision":
+            DecisionEvent decisionEvent = new DecisionEvent();
+            decisionEvent.Name = jObject["Name"]?.Value<string>() ?? throw new JsonException("Invalid Name");
+            decisionEvent.Description = jObject["Description"]?.Value<string>() ?? throw new JsonException("Invalid Description");
+            decisionEvent.Choices = jObject["Choices"]?.ToObject<Dictionary<string, Choice>>() ?? throw new JsonException("Invalid Choices");
+            decisionEvent.NodeType = "Decision";
+            return decisionEvent;
+        case "Combat":
+            CombatEvent combatEvent = new CombatEvent();
+            combatEvent.Name = jObject["Name"]?.Value<string>() ?? throw new JsonException("Invalid Name");
+            combatEvent.Description = jObject["Description"]?.Value<string>() ?? throw new JsonException("Invalid Description");
+            combatEvent.NodeType = "Combat";
+            combatEvent.CombatProcess = jObject["CombatProcess"]?.ToObject<CombatProcess>() ?? throw new JsonException("Invalid CombatProcess");
+            combatEvent.OnWin = jObject["OnWin"]?.ToObject<OnWin>() ?? throw new JsonException("Invalid OnWin");
+            combatEvent.OnLossPointer = jObject["OnLossPointer"]?.Value<string>() ?? throw new JsonException("Invalid OnLossPointer");
+            return combatEvent;
+        case "End":
+            EndEvent endEvent = new EndEvent();
+            endEvent.Name = jObject["Name"]?.Value<string>() ?? throw new JsonException("Invalid Name");
+            endEvent.Description = jObject["Description"]?.Value<string>() ?? throw new JsonException("Invalid Description");
+            endEvent.TeamDeath = jObject["TeamDeath"]?.Value<string>() ?? throw new JsonException("Invalid TeamDeath");
+            endEvent.Success = jObject["Success"]?.Value<string>() ?? throw new JsonException("Invalid Success");
+            endEvent.NodeType = "End";
+            return endEvent;
+        default:
+            throw new ArgumentException($"Invalid node type: {nodeType}");
+    }
     }
 }
-
-public static class StoryEventFactory
-{
-    public static StoryEvent CreateStoryEvent(string nodeType)
-    {
-        switch (nodeType)
-        {
-            case "Decision":
-                return new DecisionEvent();
-            case "Combat":
-                return new CombatEvent();
-            case "End":
-                return new EndEvent();
-            default:
-                throw new ArgumentException($"Invalid node type: {nodeType}");
-        }
-    }
-}
-
 
 public class DecisionEvent : StoryEvent
 {
-    // 
     public string Name { get; set; }
     public string Description { get; set; }
-    public List<Choice> Choices { get; set; }
-    public string NodeType = "Decision";
+    public Dictionary<String, Choice> Choices { get; set; }
+    public string NodeType { get; set; } = "Decision";
 
     public Choice MakeDecision()
     {
         while (true)
         {
-            Console.WriteLine("Enter your decision (as an integer):");
+            Console.WriteLine("Enter your decision:");
             string input = Console.ReadLine();
-            int decision;
-            if (int.TryParse(input, out decision))
+            if (Choices.ContainsKey(input))
             {
-                if (decision >= 0 && decision < Choices.Count)
-                {
-                    Choice choice = Choices[decision];
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid integer.");
-                }
+                Choice choice = Choices[input];
+                // Perform actions based on the chosen choice
+                return choice;
             }
             else
             {
-                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                Console.WriteLine("Invalid input. Please enter a valid decision.");
             }
         }
     }
@@ -114,17 +174,28 @@ public class Choice
 
     public string ResolveChoice(List<Agent> agents)
     {
+        Console.WriteLine("Resolving choice");
         // Calculate the multipliers for the choice, based on the agents' traits. 
         // If the positive multiplier is greater than the negative multiplier, the choice succeeds.
         List<string> agentTraits = new List<string>();
+        Console.WriteLine("Agent list:");
         foreach (Agent agent in agents)
         {
-            agentTraits.AddRange(agent.traitList.Select(trait => trait.ToString()));
+            Console.WriteLine(agent.Name);
+            if (agent.traitList != null)
+            {
+                agentTraits.AddRange(agent.traitList.Select(trait => trait.ToString()));
+            }
         }
 
         int posModifierCount = 0;
         int negModifierCount = 0;
 
+        Console.WriteLine("Agent traits:");
+        foreach (string trait in agentTraits)
+        {
+            Console.WriteLine(trait);
+        }
         foreach (string trait in agentTraits)
         {
             posModifierCount += PosModifiers.Count(modifier => modifier == trait);
@@ -134,9 +205,12 @@ public class Choice
         double posModifierMultiplier = posModifierCount * 0.1;
         double negModifierMultiplier = negModifierCount * 0.1;
 
+        Console.WriteLine($"Pos Modifier Multiplier: {posModifierMultiplier}");
+        Console.WriteLine($"Neg Modifier Multiplier: {negModifierMultiplier}");
         // Perform further calculations or actions based on the multipliers
         Random random = new Random();
         bool result = random.NextDouble() < FailureChance + negModifierMultiplier - posModifierMultiplier;
+        Console.WriteLine($"Result: {result}");
         if (result)
         {
             return SuccessPointer;
@@ -149,20 +223,6 @@ public class Choice
     }
 }
 
-public class CombatEvent: StoryEvent
-{
-    public string OnWinPointer { get; set; }
-    public List<String> RewardList { get; set; }
-    public int RewardMoney { get; set; }
-    public int RewardExp { get; set; }
-    public string OnLosePointer { get; set; }
-    public string NodeType = "Combat";
-
-    public string DebugResolveCombat()
-    {
-        return OnWinPointer;
-    }
-}
 
 public class EndEvent: StoryEvent
 {
@@ -170,9 +230,34 @@ public class EndEvent: StoryEvent
     public string Description { get; set; }
     public string TeamDeath { get; set; }
     public string Success { get; set; }
-    public string NodeType = "End";
+    public string NodeType { get; set; } = "End";
+
+    public void End()
+    {
+        Console.WriteLine("Thanks for playing!");
+    }
 }
 
+public class CombatEvent: StoryEvent
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string NodeType { get; set; } = "Combat";
+    public CombatProcess CombatProcess { get; set; }
+    public OnWin OnWin { get; set; }
+    public string OnLossPointer { get; set; }
+
+    public string DebugResolveCombat()
+    {
+        return OnWin.OnWinPointer;
+    }
+}
+
+public class OnWin
+{
+    public string OnWinPointer { get; set; }
+    public RewardList RewardList { get; set; }
+}
 
 public class CombatProcess
 {
@@ -206,13 +291,10 @@ public class Weapon
     public int Accuracy { get; set; }
 }
 
-
-public static class ParseStory
+public class RewardList
 {
-    public static EventHolder ParseStoryFile(string path)
-    {
-        string json = File.ReadAllText(path);
-        EventHolder story = JsonConvert.DeserializeObject<EventHolder>(json)!;
-        return story!;
-    }
+    public int Experience { get; set; }
+    public int Gold { get; set; }
+    public List<string> Items { get; set; }
+
 }
--- Program.cs
+++ Program.cs
@@ -10,16 +10,16 @@ namespace FixerGame
             Agent agent1 = new Agent("V", "Netrunner", 18, 4, 4, 4, 4, 4);
             agentList.Add(agent1);
 
-            string json = File.ReadAllText("data/Basic_Example.json");
-            // there will be a problem here with the return type of DeserializeStoryEvent, this needs to bean event holder
-            
-            EventHolder story = ParseStory.ParseStoryFile("data/Basic_Example.json");
+            string json = File.ReadAllText("data/No_Comments.json");
+            EventHolder story = EventHolder.LoadEventHolder(json);
             string startEventId = story.startEventId;
 
-            ProcessStory(story, startEventId, agentList);
+            Console.WriteLine("Welcome to Fixer Game!");
+            ProcStory(story, startEventId, agentList);
+            Console.WriteLine("Well that ran...");
         }
 
-        public static void ProcessStory(EventHolder story, string startEventId, List<Agent> agentList)
+        public static void ProcStory(EventHolder story, string startEventId, List<Agent> agentList)
         {
             string currentEventId = startEventId;
             string nextPointer;
@@ -27,27 +27,24 @@ namespace FixerGame
             while(currentEventId != null)
             {
                 StoryEvent currentEvent = story.Events[currentEventId];
-                switch (currentEvent.NodeType)
+                if (currentEvent is DecisionEvent decis){
+                    DecisionEvent decisionEvent = (DecisionEvent)currentEvent;
+                    Choice choice = decisionEvent.MakeDecision();
+                    nextPointer = choice.ResolveChoice(agentList);
+                    currentEventId = nextPointer;
+                } else if (currentEvent is CombatEvent comb)
                 {
-                    case "Decision":
-                        DecisionEvent decisionEvent = (DecisionEvent)currentEvent;
-                        Choice choice = decisionEvent.MakeDecision();
-                        nextPointer = choice.ResolveChoice(agentList);
-                        currentEventId = nextPointer;
-                        break;
-                    case "Combat":
-                        CombatEvent combatEvent = (CombatEvent)currentEvent;
-                        nextPointer = combatEvent.DebugResolveCombat();
-                        currentEventId = nextPointer;
-                        break;
-                    case "End":
-                        EndEvent endEvent = (EndEvent)currentEvent;
-                        // endEvent.End();
-                        
-                        currentEventId = null;
-                        break;
-                    default:
-                        throw new ArgumentException($"Invalid node type: {currentEvent.NodeType}");
+                    CombatEvent combatEvent = (CombatEvent)currentEvent;
+                    nextPointer = combatEvent.DebugResolveCombat();
+                    currentEventId = nextPointer;
+                } else if (currentEvent is EndEvent end){
+                    EndEvent endEvent = (EndEvent)currentEvent;
+                    endEvent.End();
+                    
+                    currentEventId = null;
+                } else
+                {
+                    throw new ArgumentException($"Invalid node type: {currentEvent.NodeType}");
                 }
             }
         }
